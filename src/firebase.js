9a57d5009c4601c5a3c12c6e4fa9085cc6760867
Jaagrav <jaagravseal03@gmail.com>
2021-02-10 18:20:12 +0530

    Changed firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBSO1l02gRubi7mZnIWhkxMcuXE-oMu33g",
-  authDomain: "converse-dd75d.firebaseapp.com",
-  databaseURL: "https://converse-dd75d.firebaseio.com",
-  projectId: "converse-dd75d",
-  storageBucket: "converse-dd75d.appspot.com",
-  messagingSenderId: "984079997699",
-  appId: "1:984079997699:web:3563204ba0a57051c82736",
-  measurementId: "G-MVFD08GGD1",
+  apiKey: "AIzaSyBjuzaHS-svU9tHSQTY2gr4KMnpP16iuQM",
+  authDomain: "the-coder-b3e19.firebaseapp.com",
+  databaseURL: "https://the-coder-b3e19.firebaseio.com",
+  projectId: "the-coder-b3e19",
+  storageBucket: "the-coder-b3e19.appspot.com",
+  messagingSenderId: "978497464036",
+  appId: "1:978497464036:web:818f87aaa099447306eef1",
+  measurementId: "G-LTTYCTFXXP",
 });
 
 const db = firebase.database().ref("Cordion");
